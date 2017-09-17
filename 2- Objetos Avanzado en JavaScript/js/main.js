eb1e668f5ef7b8fb1d3f398fe529f7f897c16ad7
Ismael Haytam <anonismaa@gmail.com>
2017-09-17 19:06:22 +0200

    Agrega métodos para mover los puntos

---
+++ 2- Objetos Avanzado en JavaScript/js/main.js
@@ -0,0 +1,28 @@
+
+const p1 = {
+    x: 0,
+    y: 4,
+    moverEnX: function (x) { this.x += x },
+    moverEnY: function (y) {this.y += y}
+}
+
+const p2 = {
+    x: 3,
+    y: 0,
+    moverEnX: function (x) { this.x += x },
+    moverEnY: function (y) {this.y += y}
+}
+
+function distancia(p1, p2) {
+    const x = p1.x - p2.x
+    const y = p1.y - p2.y
+
+    return Math.sqrt(x * x + y * y)
+}
+
+console.log(distancia(p1, p2));
+
+p1.moverEnX(3);
+p2.moverEnX(3);
+
+console.log(distancia(p1, p2));
