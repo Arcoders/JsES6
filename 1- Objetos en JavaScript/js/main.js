10e7a37a43edcd9630f8eea33cb38990e11e6845
Ismael Haytam <anonismaa@gmail.com>
2017-09-17 18:53:01 +0200

    Calcular la distancia entre dos puntos - Objetos en JavaScript

---
+++ 1- Objetos en JavaScript/js/main.js
@@ -0,0 +1,19 @@
+
+const p1 = {
+    x: 0,
+    y: 4
+}
+
+const p2 = {
+    x: 3,
+    y: 0
+}
+
+function distancia(p1, p2) {
+    const x = p1.x - p2.x
+    const y = p1.y - p2.y
+
+    return Math.sqrt(x * x + y * y)
+}
+
+console.log(distancia(p1, p2));
