cad4aa7ba74ccfdc3d08bd1ba9ad6ed4f28af1bc
Ismael Haytam <anonismaa@gmail.com>
2017-09-19 19:43:53 +0200

    this y los arrow functions

--- 6- this y los arrow/js/main.js
+++ 6- this y los arrow/js/main.js
@@ -1,12 +1,16 @@
 
-const suma = (...numeros) => numeros.reduce((acum, numero) => {
-    return acum += numero
-},0);
+class Persona {
 
-const dobles = (...numeros) => numeros.map(numero => numero * 2)
+    constructor(nombre, amigos = []) {
+        this.nombre = nombre;
+        this.amigos = amigos;
+    }
 
-const pares = (...numeros) => numeros.filter(numero => numero % 2 == 0)
+    listarAmigos() {
+        this.amigos.forEach(amigo => console.log(`${this.nombre} amigo de ${amigo}`));
+    }
 
-console.log(suma(2, 5, 6, 8));
-console.log(dobles(9, 7, 3, 1));
-console.log(pares(3, 4, 10, 12));
+}
+
+const ismael = new Persona('Ismael', ['Moha', 'victor']);
+ismael.listarAmigos();
